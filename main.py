e995745678b311f569a0bd25a48ddf923c814c03
Florian <florian.luna@gmail.com>
2018-03-10 19:31:07 -0500

    first implementation still needing some changes

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import array
 import rt
 
+# a simple color method
+def Color(ray):
+	unitDirection = rt.UnitVector(ray.Direction)
+	t = 0.5 * (unitDirection.Y + 1.0)
+	tmp = rt.Vec3(1.0,1.0,1.0)
+	tmp2 = rt.Vec3(0.5,0.7,1.0)
+	return tmp.Lerp(tmp2,t)
+
 # PPM header
-width = 200
-height = 100
+nx = 200
+ny = 100
 maxval = 255
-ppm_header = f'P6 {width} {height} {maxval}\n'
+ppm_header = f'P6 {nx} {ny} {maxval}\n'
 
-# PPM image data (filled with blue)
-image = array.array('B', [0, 0, 0] * width * height)
+# PPM image data
+image = array.array('B', [0, 0, 0] * nx * ny)
 
-# Fill with red the rectangle with origin at (10, 10) and width x height = 50 x 80 pixels
-for i in range(0, width-1):
-	for j in range(0, height-1):
-		index = 3 * ((height-1-j) * width + i) #matching what is in the book
-		image[index] = int(float(i)/float(width)*255.0)
-		image[index + 1] = int(float(j)/float(height)*255.0)
-		image[index + 2] = int(0.2*255.0)
+lowerLeftCorner = rt.Vec3(-2.0,-1.0,-1.0)
+horizontal = rt.Vec3(4.0,0.0,0.0)
+vertical = rt.Vec3(0.0,2.0,0.0)
+origin = rt.Vec3(0.0,0.0,0.0)
+
+for i in range(0, nx-1):
+	for j in range(0, ny-1):
+		u = float(i) / float(nx)
+		v = float(i) / float(ny)
+		ray = rt.Ray(origin, lowerLeftCorner+horizontal.Mul(u)+vertical.Mul(v))
+		color = Color(ray)
+		index = 3 * ((ny-1-j) * nx + i) #matching what is in the book
+		image[index] = int(color.X*255.99)
+		image[index + 1] = int(color.Y*255.99)
+		image[index + 2] = int(color.Y*255.99)
 
 test = rt.Vec3(1.0, 2.0, 3.0)
 
 # Save the PPM image as a binary file
-with open('blue_red_example.ppm', 'wb') as f:
+with open('raytracing.ppm', 'wb') as f:
 	f.write(bytearray(ppm_header, 'ascii'))
 	image.tofile(f)
--- rt.py
+++ rt.py
@@ -1,21 +1,62 @@
-#import math
+import math
 
 class Vec3:
-	x=0.0
-	y=0.0
-	z=0.0
-	
-	def __init__(self, ax, ay, az):
-		self.x=ax
-		self.y=ay
-		self.z=az
+	X=0.0
+	Y=0.0
+	Z=0.0
+
+	def __init__(self, x, y, z):
+		self.X=x
+		self.Y=y
+		self.Z=z
 
 	def __add__(self, other):
-		return vec3(self.__x+other.__x, self.__y+other.__y, self.__z+other.__z,)
+		return Vec3(self.X+other.X, self.Y+other.Y, self.Z+other.Z)
 
 	def __sub__(self, other):
-		return vec3(self.__x-other.__x, self.__y-other.__y, self.__z-other.__z,)
+		return Vec3(self.X-other.X, self.Y-other.Y, self.Z-other.Z)
 
-	def Dot(other):
-		return self.__x*other.__x + self.__y*other.__y + self.__z*other.__z
+	def __mul__(self, other):
+		return Vec3(self.X*other.X, self.Y*other.Y, self.Z*other.Z)
+
+	def Mul(self, value):
+		return Vec3(self.X*value, self.Y*value, self.Z*value)
+
+	def __div__(self, other):
+		return Vec3(self.X/other.X, self.Y/other.Y, self.Z/other.Z)
+
+	def Dot(self,other):
+		return self.X*other.X + self.Y*other.Y + self.Z*other.Z
+
+	def Length(self):
+		return math.sqrt(self.X*self.X + self.Y*self.Y + self.Z*self.Z)
+
+	def Lerp(self,other, t):
+		return self.Mul(1.0-t) + other.Mul(t)
+
+def UnitVector(myVector):
+	result = Vec3(0.0,0.0,0.0)
+	length = myVector.Length()
+	if length > 0.0 :
+		result = myVector.Mul(1.0/length)
+	return result
+
+
+class Ray : 
+	Origin = Vec3(0.0,0.0,0.0)
+	Direction = Vec3(0.0,0.0,0.0)
+
+	def __init__(self, origin, direction):
+		Origin = origin
+		Direction = direction
+
+	def PointAtParameter(t):
+		return Origin + direction.Mul(t) #TODO : probably not the right way to multiply a vec3 with a float
+
+
+#def Color(Ray ray):
+
+
+
+
 
